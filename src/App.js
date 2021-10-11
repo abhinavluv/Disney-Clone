86153e7f1757496033d0615e75570ea25b95617b
Abhinav <abhilav.sharma19@gmail.com>
2021-10-11 22:17:40 +0530

    Movie Detail page layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 import './App.css';
+import Detail from './components/Detail.component';
 import Header from './components/Header.component';
 import Home from './components/Home.component';
 
 function App() {
     return (
         <div className='App'>
-            <Header />
-            <Home />
+            <Router>
+                <Header />
+                <Switch>
+                    <Route path='/' exact>
+                        <Home />
+                    </Route>
+                    <Route path='/detail' exact>
+                        <Detail />
+                    </Route>
+                </Switch>
+            </Router>
         </div>
     );
 }
